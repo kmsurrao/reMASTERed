1f0f32280059e93b24fb3b52a5413323cfb6206e
Kristen M Surrao <kms2320@bake.cm.cluster>
2022-12-26 12:01:45 -0500

    initialize temp array

--- wigner3j.py
+++ wigner3j.py
@@ -11,6 +11,8 @@ def compute_3j_symbol_one_ell(l1, ellmax):
     RETURNS
     wig3j_one_ell: 2D numpy array, indexed as wig3j_one_ell[l2,l3], contains all 3j symbols for l1
     '''
+    wig.wig_table_init(ellmax*2,9)
+    wig.wig_temp_init(ellmax*2)
     tj0 = lambda l1,l2,l3: wig.wig3jj(2*l1,2*l2,2*l3,0,0,0)
     wig3j_one_ell = np.zeros((ellmax+1, ellmax+1), dtype=np.float32)
     for l2 in range(ellmax+1):
@@ -28,8 +30,6 @@ def compute_3j(ellmax):
     RETURNS
     wig3j: 3D numpy array, contains wigner 3j symbols, indexed as wig3j[l1,l2,l3]
     '''
-    wig.wig_table_init(ellmax*2,9)
-    wig.wig_temp_init(ellmax*2)
     pool = mp.Pool(16)
     wig3j = pool.starmap(compute_3j_symbol_one_ell, [(l1, ellmax) for l1 in range(ellmax+1)])
     pool.close()
